2b6cdfe2f017382319452993b0a2512ac827924b
dvidaletti <dvidaletti@stefanini.com>
2018-12-06 17:48:24 -0200

    sequelize

--- src/models/UserModels.ts
+++ src/models/UserModels.ts
@@ -2,6 +2,8 @@ import * as Sequelize from "sequelize";
 import { BaseModelInterface } from "../interfaces/BaseModelInterface";
 // src/typings/SequelizeAttributes/index.d.ts
 import { DataTypeAbstract, DefineAttributeColumnOptions } from "sequelize";
+import { genSaltSync, hash, hashSync, compareSync } from "bcryptjs";
+import { ModelsInterface } from "../interfaces/ModelsInterface";
 
 type SequelizeAttribute = string | DataTypeAbstract | DefineAttributeColumnOptions;
 
@@ -13,7 +15,9 @@ export interface UserAttributes {
   id?: any;
   name: string;
   email: string;
-  password?: string;
+  password: string;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export interface UserInstance extends Sequelize.Instance<UserAttributes>, UserAttributes {
@@ -32,11 +36,35 @@ export default (sequelize: Sequelize.Sequelize, DataType: Sequelize.DataTypes): 
     },
     name: {
       type: DataType.STRING(127),
+      allowNull: false,
     },
     email: {
-      type: DataType.STRING(127)
+      type: DataType.STRING(127),
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataType.STRING(128),
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
     }
   };
-  const user = sequelize.define<UserInstance, UserAttributes>('User', attr);
+  const user: UserModel = sequelize.define<UserInstance, UserAttributes>('User', attr, {
+    tableName: "users",
+    hooks: {
+      beforeCreate: (user: UserInstance, options: Sequelize.CreateOptions): void => {
+        const salt = genSaltSync();
+        user.password = hashSync(user.password, salt);
+      }
+    }
+  });
+
+  user.associate = (models: ModelsInterface) => { };
+
+  user.prototype.isPassword = (encodePassword: string, password: string): boolean => {
+    return compareSync(password, encodePassword);
+  }
   return user;
 }
