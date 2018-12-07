8fd3dfd19dbcc6ba8667058b7e489b53df6cb5aa
dvidaletti <dvidaletti@stefanini.com>
2018-12-07 12:36:16 -0200

    removendo typescriot

---
+++ src/config/Server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator')();
+var expressSession = require('express-session');
+// var allowCors = require('./cors');
+var app = express();
+
+//permitir codificação url
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+//app.use(bodyParser.json());
+app.use(expressValidator);
+// app.use(allowCors)
+
+app.use(expressSession({
+    secret: 'xpto', //chave de segurança
+    resave: true, //regravada no servidor
+    saveUninitialized: false, //sera regravada se for modificada,
+    cookie: {
+        secure: false,
+    }
+}));
+
+app.use(express.static('./app/public'));
+
+//autoload dos arquivos de rotas e conexao
+consign()
+    // .include('app/routes')
+    // .then('config/mongodb.js')
+    .then('app/models')
+    // .then('app/controllers')
+    .into(app)
+
+module.exports = app;
--- src/interfaces/ModelsInterface.ts
+++
@@ -1,5 +0,0 @@
-import { UserModel } from "../models/UserModels";
-
-export interface ModelsInterface {
-  User: UserModel
-}
